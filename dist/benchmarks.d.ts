8be99820f6307aab4e19ba005a430b22497967d0
Nick Springer <springer.268@osu.edu>
2023-05-01 22:21:36 -0700

    q

---
+++ dist/benchmarks.d.ts
@@ -0,0 +1,119 @@
+/// <reference types="node" />
+import { CallOptions, ChannelCredentials, Client, ClientOptions, ClientReadableStream, ClientUnaryCall, handleServerStreamingCall, handleUnaryCall, Metadata, ServiceError, UntypedServiceImplementation } from "@grpc/grpc-js";
+import _m0 from "protobufjs/minimal";
+export interface ServerInfo {
+    server: string;
+}
+export interface VmList {
+    vms: string[];
+}
+export interface BenchmarkMessage {
+    text: string;
+}
+export interface voidNoParam {
+}
+export declare const ServerInfo: {
+    encode(message: ServerInfo, writer?: _m0.Writer): _m0.Writer;
+    decode(input: _m0.Reader | Uint8Array, length?: number): ServerInfo;
+    fromJSON(object: any): ServerInfo;
+    toJSON(message: ServerInfo): unknown;
+    create<I extends {
+        server?: string | undefined;
+    } & {
+        server?: string | undefined;
+    } & { [K in Exclude<keyof I, "server">]: never; }>(base?: I | undefined): ServerInfo;
+    fromPartial<I_1 extends {
+        server?: string | undefined;
+    } & {
+        server?: string | undefined;
+    } & { [K_1 in Exclude<keyof I_1, "server">]: never; }>(object: I_1): ServerInfo;
+};
+export declare const VmList: {
+    encode(message: VmList, writer?: _m0.Writer): _m0.Writer;
+    decode(input: _m0.Reader | Uint8Array, length?: number): VmList;
+    fromJSON(object: any): VmList;
+    toJSON(message: VmList): unknown;
+    create<I extends {
+        vms?: string[] | undefined;
+    } & {
+        vms?: (string[] & string[] & { [K in Exclude<keyof I["vms"], keyof string[]>]: never; }) | undefined;
+    } & { [K_1 in Exclude<keyof I, "vms">]: never; }>(base?: I | undefined): VmList;
+    fromPartial<I_1 extends {
+        vms?: string[] | undefined;
+    } & {
+        vms?: (string[] & string[] & { [K_2 in Exclude<keyof I_1["vms"], keyof string[]>]: never; }) | undefined;
+    } & { [K_3 in Exclude<keyof I_1, "vms">]: never; }>(object: I_1): VmList;
+};
+export declare const BenchmarkMessage: {
+    encode(message: BenchmarkMessage, writer?: _m0.Writer): _m0.Writer;
+    decode(input: _m0.Reader | Uint8Array, length?: number): BenchmarkMessage;
+    fromJSON(object: any): BenchmarkMessage;
+    toJSON(message: BenchmarkMessage): unknown;
+    create<I extends {
+        text?: string | undefined;
+    } & {
+        text?: string | undefined;
+    } & { [K in Exclude<keyof I, "text">]: never; }>(base?: I | undefined): BenchmarkMessage;
+    fromPartial<I_1 extends {
+        text?: string | undefined;
+    } & {
+        text?: string | undefined;
+    } & { [K_1 in Exclude<keyof I_1, "text">]: never; }>(object: I_1): BenchmarkMessage;
+};
+export declare const voidNoParam: {
+    encode(_: voidNoParam, writer?: _m0.Writer): _m0.Writer;
+    decode(input: _m0.Reader | Uint8Array, length?: number): voidNoParam;
+    fromJSON(_: any): voidNoParam;
+    toJSON(_: voidNoParam): unknown;
+    create<I extends {} & {} & { [K in Exclude<keyof I, never>]: never; }>(base?: I | undefined): voidNoParam;
+    fromPartial<I_1 extends {} & {} & { [K_1 in Exclude<keyof I_1, never>]: never; }>(_: I_1): voidNoParam;
+};
+export type BenchmarksService = typeof BenchmarksService;
+export declare const BenchmarksService: {
+    readonly runKBench: {
+        readonly path: "/benchmarks.Benchmarks/runKBench";
+        readonly requestStream: false;
+        readonly responseStream: true;
+        readonly requestSerialize: (value: voidNoParam) => Buffer;
+        readonly requestDeserialize: (value: Buffer) => voidNoParam;
+        readonly responseSerialize: (value: BenchmarkMessage) => Buffer;
+        readonly responseDeserialize: (value: Buffer) => BenchmarkMessage;
+    };
+    readonly sayHello: {
+        readonly path: "/benchmarks.Benchmarks/sayHello";
+        readonly requestStream: false;
+        readonly responseStream: false;
+        readonly requestSerialize: (value: voidNoParam) => Buffer;
+        readonly requestDeserialize: (value: Buffer) => voidNoParam;
+        readonly responseSerialize: (value: BenchmarkMessage) => Buffer;
+        readonly responseDeserialize: (value: Buffer) => BenchmarkMessage;
+    };
+    readonly getVMs: {
+        readonly path: "/benchmarks.Benchmarks/getVMs";
+        readonly requestStream: false;
+        readonly responseStream: false;
+        readonly requestSerialize: (value: ServerInfo) => Buffer;
+        readonly requestDeserialize: (value: Buffer) => ServerInfo;
+        readonly responseSerialize: (value: VmList) => Buffer;
+        readonly responseDeserialize: (value: Buffer) => VmList;
+    };
+};
+export interface BenchmarksServer extends UntypedServiceImplementation {
+    runKBench: handleServerStreamingCall<voidNoParam, BenchmarkMessage>;
+    sayHello: handleUnaryCall<voidNoParam, BenchmarkMessage>;
+    getVMs: handleUnaryCall<ServerInfo, VmList>;
+}
+export interface BenchmarksClient extends Client {
+    runKBench(request: voidNoParam, options?: Partial<CallOptions>): ClientReadableStream<BenchmarkMessage>;
+    runKBench(request: voidNoParam, metadata?: Metadata, options?: Partial<CallOptions>): ClientReadableStream<BenchmarkMessage>;
+    sayHello(request: voidNoParam, callback: (error: ServiceError | null, response: BenchmarkMessage) => void): ClientUnaryCall;
+    sayHello(request: voidNoParam, metadata: Metadata, callback: (error: ServiceError | null, response: BenchmarkMessage) => void): ClientUnaryCall;
+    sayHello(request: voidNoParam, metadata: Metadata, options: Partial<CallOptions>, callback: (error: ServiceError | null, response: BenchmarkMessage) => void): ClientUnaryCall;
+    getVMs(request: ServerInfo, callback: (error: ServiceError | null, response: VmList) => void): ClientUnaryCall;
+    getVMs(request: ServerInfo, metadata: Metadata, callback: (error: ServiceError | null, response: VmList) => void): ClientUnaryCall;
+    getVMs(request: ServerInfo, metadata: Metadata, options: Partial<CallOptions>, callback: (error: ServiceError | null, response: VmList) => void): ClientUnaryCall;
+}
+export declare const BenchmarksClient: {
+    new (address: string, credentials: ChannelCredentials, options?: Partial<ClientOptions>): BenchmarksClient;
+    service: typeof BenchmarksService;
+};
---
+++ dist/benchmarks.js
@@ -0,0 +1,231 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BenchmarksClient = exports.BenchmarksService = exports.voidNoParam = exports.BenchmarkMessage = exports.VmList = exports.ServerInfo = void 0;
+const grpc_js_1 = require("@grpc/grpc-js");
+const minimal_1 = __importDefault(require("protobufjs/minimal"));
+function createBaseServerInfo() {
+    return { server: "" };
+}
+exports.ServerInfo = {
+    encode(message, writer = minimal_1.default.Writer.create()) {
+        if (message.server !== "") {
+            writer.uint32(10).string(message.server);
+        }
+        return writer;
+    },
+    decode(input, length) {
+        const reader = input instanceof minimal_1.default.Reader ? input : minimal_1.default.Reader.create(input);
+        let end = length === undefined ? reader.len : reader.pos + length;
+        const message = createBaseServerInfo();
+        while (reader.pos < end) {
+            const tag = reader.uint32();
+            switch (tag >>> 3) {
+                case 1:
+                    if (tag != 10) {
+                        break;
+                    }
+                    message.server = reader.string();
+                    continue;
+            }
+            if ((tag & 7) == 4 || tag == 0) {
+                break;
+            }
+            reader.skipType(tag & 7);
+        }
+        return message;
+    },
+    fromJSON(object) {
+        return { server: isSet(object.server) ? String(object.server) : "" };
+    },
+    toJSON(message) {
+        const obj = {};
+        message.server !== undefined && (obj.server = message.server);
+        return obj;
+    },
+    create(base) {
+        return exports.ServerInfo.fromPartial(base !== null && base !== void 0 ? base : {});
+    },
+    fromPartial(object) {
+        var _a;
+        const message = createBaseServerInfo();
+        message.server = (_a = object.server) !== null && _a !== void 0 ? _a : "";
+        return message;
+    },
+};
+function createBaseVmList() {
+    return { vms: [] };
+}
+exports.VmList = {
+    encode(message, writer = minimal_1.default.Writer.create()) {
+        for (const v of message.vms) {
+            writer.uint32(10).string(v);
+        }
+        return writer;
+    },
+    decode(input, length) {
+        const reader = input instanceof minimal_1.default.Reader ? input : minimal_1.default.Reader.create(input);
+        let end = length === undefined ? reader.len : reader.pos + length;
+        const message = createBaseVmList();
+        while (reader.pos < end) {
+            const tag = reader.uint32();
+            switch (tag >>> 3) {
+                case 1:
+                    if (tag != 10) {
+                        break;
+                    }
+                    message.vms.push(reader.string());
+                    continue;
+            }
+            if ((tag & 7) == 4 || tag == 0) {
+                break;
+            }
+            reader.skipType(tag & 7);
+        }
+        return message;
+    },
+    fromJSON(object) {
+        return { vms: Array.isArray(object === null || object === void 0 ? void 0 : object.vms) ? object.vms.map((e) => String(e)) : [] };
+    },
+    toJSON(message) {
+        const obj = {};
+        if (message.vms) {
+            obj.vms = message.vms.map((e) => e);
+        }
+        else {
+            obj.vms = [];
+        }
+        return obj;
+    },
+    create(base) {
+        return exports.VmList.fromPartial(base !== null && base !== void 0 ? base : {});
+    },
+    fromPartial(object) {
+        var _a;
+        const message = createBaseVmList();
+        message.vms = ((_a = object.vms) === null || _a === void 0 ? void 0 : _a.map((e) => e)) || [];
+        return message;
+    },
+};
+function createBaseBenchmarkMessage() {
+    return { text: "" };
+}
+exports.BenchmarkMessage = {
+    encode(message, writer = minimal_1.default.Writer.create()) {
+        if (message.text !== "") {
+            writer.uint32(10).string(message.text);
+        }
+        return writer;
+    },
+    decode(input, length) {
+        const reader = input instanceof minimal_1.default.Reader ? input : minimal_1.default.Reader.create(input);
+        let end = length === undefined ? reader.len : reader.pos + length;
+        const message = createBaseBenchmarkMessage();
+        while (reader.pos < end) {
+            const tag = reader.uint32();
+            switch (tag >>> 3) {
+                case 1:
+                    if (tag != 10) {
+                        break;
+                    }
+                    message.text = reader.string();
+                    continue;
+            }
+            if ((tag & 7) == 4 || tag == 0) {
+                break;
+            }
+            reader.skipType(tag & 7);
+        }
+        return message;
+    },
+    fromJSON(object) {
+        return { text: isSet(object.text) ? String(object.text) : "" };
+    },
+    toJSON(message) {
+        const obj = {};
+        message.text !== undefined && (obj.text = message.text);
+        return obj;
+    },
+    create(base) {
+        return exports.BenchmarkMessage.fromPartial(base !== null && base !== void 0 ? base : {});
+    },
+    fromPartial(object) {
+        var _a;
+        const message = createBaseBenchmarkMessage();
+        message.text = (_a = object.text) !== null && _a !== void 0 ? _a : "";
+        return message;
+    },
+};
+function createBasevoidNoParam() {
+    return {};
+}
+exports.voidNoParam = {
+    encode(_, writer = minimal_1.default.Writer.create()) {
+        return writer;
+    },
+    decode(input, length) {
+        const reader = input instanceof minimal_1.default.Reader ? input : minimal_1.default.Reader.create(input);
+        let end = length === undefined ? reader.len : reader.pos + length;
+        const message = createBasevoidNoParam();
+        while (reader.pos < end) {
+            const tag = reader.uint32();
+            switch (tag >>> 3) {
+            }
+            if ((tag & 7) == 4 || tag == 0) {
+                break;
+            }
+            reader.skipType(tag & 7);
+        }
+        return message;
+    },
+    fromJSON(_) {
+        return {};
+    },
+    toJSON(_) {
+        const obj = {};
+        return obj;
+    },
+    create(base) {
+        return exports.voidNoParam.fromPartial(base !== null && base !== void 0 ? base : {});
+    },
+    fromPartial(_) {
+        const message = createBasevoidNoParam();
+        return message;
+    },
+};
+exports.BenchmarksService = {
+    runKBench: {
+        path: "/benchmarks.Benchmarks/runKBench",
+        requestStream: false,
+        responseStream: true,
+        requestSerialize: (value) => Buffer.from(exports.voidNoParam.encode(value).finish()),
+        requestDeserialize: (value) => exports.voidNoParam.decode(value),
+        responseSerialize: (value) => Buffer.from(exports.BenchmarkMessage.encode(value).finish()),
+        responseDeserialize: (value) => exports.BenchmarkMessage.decode(value),
+    },
+    sayHello: {
+        path: "/benchmarks.Benchmarks/sayHello",
+        requestStream: false,
+        responseStream: false,
+        requestSerialize: (value) => Buffer.from(exports.voidNoParam.encode(value).finish()),
+        requestDeserialize: (value) => exports.voidNoParam.decode(value),
+        responseSerialize: (value) => Buffer.from(exports.BenchmarkMessage.encode(value).finish()),
+        responseDeserialize: (value) => exports.BenchmarkMessage.decode(value),
+    },
+    getVMs: {
+        path: "/benchmarks.Benchmarks/getVMs",
+        requestStream: false,
+        responseStream: false,
+        requestSerialize: (value) => Buffer.from(exports.ServerInfo.encode(value).finish()),
+        requestDeserialize: (value) => exports.ServerInfo.decode(value),
+        responseSerialize: (value) => Buffer.from(exports.VmList.encode(value).finish()),
+        responseDeserialize: (value) => exports.VmList.decode(value),
+    },
+};
+exports.BenchmarksClient = (0, grpc_js_1.makeGenericClientConstructor)(exports.BenchmarksService, "benchmarks.Benchmarks");
+function isSet(value) {
+    return value !== null && value !== undefined;
+}
+//# sourceMappingURL=benchmarks.js.map
